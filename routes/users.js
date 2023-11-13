48eded5ee11368a2dd7ed875d44c11f0accaf67d
Juanan Pereira <juanan.pereira@ehu.eus>
2023-11-13 12:39:19 +0100

    Add user authentication using database.json

--- routes/users.js
+++ routes/users.js
@@ -6,20 +6,24 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-//username and password
-const myusername = 'user1'
-const mypassword = 'mypassword'
+// load database.json
+const datubase = require('./datubase.json');
 
 router.post('/',(req,res) => {
-  if(req.body.username == myusername && req.body.password == mypassword){
 
-    req.session.userid=req.body.username;
-    console.log(req.session)
+  datubase.forEach(function(user){
+    if(req.body.username == user.username && req.body.password == user.password){
+      req.session.userid=req.body.username;
+      console.log(req.session)
+    }
+  })
+
+  if (req.session.userid) {
     res.redirect('/protected');
-  }
-  else{
+  } else {
     res.send('Invalid username or password');
   }
+
 })
 
 
